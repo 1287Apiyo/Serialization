9a00315fb51781dcb4d6488e9659d2947deee4cf
Anne Apiyo <anneapiyo0@gmail.com>
2024-03-10 15:43:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //Serialization-The process of converting an object into a byte stream
+        //It is mainly used in Hibernate, RMI, JPA, EJB and JMS technologies.
+        //For serializing the object, we call the writeObject() method of ObjectOutputStream class, and for deserialization we call the readObject() method of ObjectInputStream class.
+        //This byte stream can be saved as a file or sent over a network
+        //Can be sent to different machines
+        //Byte file can be saved as a file(.ser)which is platform independent
+
+        //Deserialization-reverse of converting byte stream into objects
+        
+    }
+}
